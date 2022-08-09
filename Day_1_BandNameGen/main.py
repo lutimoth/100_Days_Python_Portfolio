d99a9d1878ccd04748fa538cf22c410e22bb3ce7
naturesbless <lutimoth@usc.edu>
2022-08-09 00:35:17 -0700

    Creating BandNameGenerator

---
+++ Day_1_BandNameGen/main.py
@@ -0,0 +1,13 @@
+# takes in an input and outputs the output concatenated with the input
+# print("Hello "+input("What is your name?"))
+
+# Greeting for the program.
+print("Welcome to the simple band name generator.")
+
+# Assign variable to city, use \n to let input be on separate line
+city = input("What's the name of the city you grew up in?\n")
+
+# Get name of their pet
+pet = input("What is the name of your favorite pet?\n")
+
+print("Your band name is: " + city + " " + pet)
