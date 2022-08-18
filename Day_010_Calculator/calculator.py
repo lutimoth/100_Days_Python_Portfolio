cdbf174ac0995fa68c1ed429cf2b59f3b5099ad5
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-08-18 02:07:07 -0700

    Finish Basic Calculator

--- Day_010_Calculator/calculator.py
+++ Day_010_Calculator/calculator.py
@@ -23,15 +23,29 @@ operations = {
     "/": div
 }
 
-num1 = int(input("What is the first number?: "))
-num2 = int(input("What is the second number?: "))
 
-for symbol in operations:
-    print(symbol)
-    
-operation_symbol = input("Pick an operation from the line above: ") 
+def calculator():
+    print(logo)
 
-func = operations[operation_symbol]
-answer = func(num1, num2)
+    num1 = float(input("What is the first number?: "))
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+    for symbol in operations:
+        print(symbol)
+
+    more_math = True
+
+    while more_math:
+        operation_symbol = input("Pick an operation: ") 
+        num2 = float(input("What is the second number?: "))
+        func = operations[operation_symbol]
+        answer = func(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}: ") == 'y':
+            num1 = answer
+        else:
+            more_math = False
+            calculator()
+
+calculator()
