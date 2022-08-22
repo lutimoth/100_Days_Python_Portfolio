defedc2e4b740e502005a92e836eb8542604e0eb
naturesbless <lutimoth@usc.edu>
2022-08-21 22:59:04 -0700

    Fix 5th bug

--- Day_12_DebuggingPractice/debuggingpractice.py
+++ Day_12_DebuggingPractice/debuggingpractice.py
@@ -29,7 +29,8 @@
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# print(pages)
+# word_per_page = int(input("Number of words per page: "))
 # total_words = pages * word_per_page
 # print(total_words)
 
