7d422ff6630b3af2b026f2013521928a7504450d
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-08-25 21:25:47 -0700

    Create object tutorial and upload quiz fiels

---
+++ Day_017_TriviaGame/start.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+                
+user = User("001", "test_user")
+
+print(user.id, user.username)
