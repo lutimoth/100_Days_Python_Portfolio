ee1f093e2cce4e707593b9e64eb9a48f9c580bc4
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-08-25 21:49:31 -0700

    Fixed score attribute

--- Day_017_TriviaGame/quiz_brain.py
+++ Day_017_TriviaGame/quiz_brain.py
@@ -5,7 +5,7 @@ class QuizBrain:
     def __init__(self, q_list):
         self.question_number = 0
         self.question_list = q_list
-        score = 0
+        self.score = 0
         
     def next_question(self):
         current_question = self.question_list[self.question_number]
--- Day_017_TriviaGame/start.py
+++ Day_017_TriviaGame/start.py
@@ -2,6 +2,9 @@ class User:
     def __init__(self, user_id, username):
         self.id = user_id
         self.username = username
+     
+        
+    
                 
 user = User("001", "test_user")
 
