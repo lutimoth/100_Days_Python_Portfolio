8df428f25dafe63b7446a190248c442ad23f6ffd
naturesbless <lutimoth@usc.edu>
2022-08-27 01:52:12 -0700

    Create centered and balanced painting

--- Day_018_TurtleHirst/main.py
+++ Day_018_TurtleHirst/main.py
@@ -22,34 +22,40 @@ color_list = rgb_colors[3:]
 hirst = t.Turtle()
 hirst.speed(10)
 hirst.penup()
+hirst.hideturtle()
 
 #hirst.setpos(length * -1, width * -1)
 
-# Draw a hirst painting of a set length (x) and width(y)
-# Must be int
-# Will take lenght and width as nested for loop and set length as x and width as y
+# Draw a square Hirst painting of length x
+# Will move turtle to create a centered painting 
+# Fixed spacing between dots
+
 def draw_hirst(num_dots, length):
-    hirst.setpos(length*-1, length*-1)
+    num_rows = num_dots/length # Calculate how many rows of dots for overal height
+    width = length//2 * 50 * -1 # Calculate width in dots, divided by 2 for balanced left and right
+    height = num_rows//2 * 50 * -1 # Divide number of rows in 2 for balanced height
+
+    hirst.setpos(width, height)
 
     for dot_count in range(1, num_dots + 1):
         hirst.dot(20, random.choice(color_list))
         hirst.forward(50)
 
-        if dot_count % 10 == 0:
+        if dot_count % length == 0:
             hirst.setheading(90)
             hirst.forward(50)
             hirst.setheading(180)
-            hirst.forward(500)
+            hirst.forward(length*50)
             hirst.setheading(0)
 
-draw_hirst(100, 10)
+draw_hirst(100, 20)
         
     # for y in range(width):
     #     for x in range(length):
     #         hirst.setpos(x*30, y*30)
     #         hirst.dot(20, random.choice(color_list))
         
-draw_hirst(20, 20)
+#draw_hirst(20, 20)
 
 # for color in color_list:
 #     hirst.dot(20, color)
