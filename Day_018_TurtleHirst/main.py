3933301981c165a4dc55e928975aebd1b07dde53
naturesbless <lutimoth@usc.edu>
2022-08-27 01:39:36 -0700

    Create iterative shape

--- Day_018_TurtleHirst/main.py
+++ Day_018_TurtleHirst/main.py
@@ -23,16 +23,31 @@ hirst = t.Turtle()
 hirst.speed(10)
 hirst.penup()
 
+#hirst.setpos(length * -1, width * -1)
+
 # Draw a hirst painting of a set length (x) and width(y)
 # Must be int
 # Will take lenght and width as nested for loop and set length as x and width as y
-def draw_hirst(length, width):
-    hirst.setpos(length * -1, width * -1)
+def draw_hirst(num_dots, length):
+    hirst.setpos(length*-1, length*-1)
 
-    for y in range(width):
-        for x in range(length):
-            hirst.setpos(x*30, y*30)
-            hirst.dot(20, random.choice(color_list))
+    for dot_count in range(1, num_dots + 1):
+        hirst.dot(20, random.choice(color_list))
+        hirst.forward(50)
+
+        if dot_count % 10 == 0:
+            hirst.setheading(90)
+            hirst.forward(50)
+            hirst.setheading(180)
+            hirst.forward(500)
+            hirst.setheading(0)
+
+draw_hirst(100, 10)
+        
+    # for y in range(width):
+    #     for x in range(length):
+    #         hirst.setpos(x*30, y*30)
+    #         hirst.dot(20, random.choice(color_list))
         
 draw_hirst(20, 20)
 
