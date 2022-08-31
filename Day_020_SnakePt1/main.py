c2c35db935236ef7ed609e32c979c85b0ba29960
naturesbless <lutimoth@usc.edu>
2022-08-30 23:04:35 -0700

    Create snake and initial food object

--- Day_020_SnakePt1/main.py
+++ Day_020_SnakePt1/main.py
@@ -1,6 +1,7 @@
-from turtle import Screen, Turtle, tracer
+from turtle import Screen
 import time
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.setup(width = 600, height = 600)
@@ -11,19 +12,25 @@ screen.title("Snake-y Snake Time")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 gaming = True
-
 while gaming:
     # Keep update outside for loop so we only update when all 3 segments moved
     screen.update()
     time.sleep(0.1)
-    
     # for seg in segments: This is not good
     #     seg.forward(20)  Prevents turning
-
     snake.move()
     
-    # Move the last segment (head) forward
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        print("nom nom nom")
 
 screen.exitonclick()
