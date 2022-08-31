dfdbb88e8bb2c24ce16a67403e6493593aa1325b
naturesbless <lutimoth@usc.edu>
2022-08-30 23:22:00 -0700

    Create Food generation and Scoreboard
    Created Food method for refreshing
    Created Scoreboard object for tracking score and printing on screen

--- Day_020_SnakePt1/food.py
+++ Day_020_SnakePt1/food.py
@@ -14,3 +14,9 @@ class Food(Turtle):
         random_x = random.randint(-200,200)
         random_y = random.randint(-200,200)
         self.goto(random_x, random_y)
+        self.refresh()
+
+    def refresh(self):
+        random_x = random.randint(-200,200)
+        random_y = random.randint(-200,200)
+        self.goto(random_x, random_y)
---
+++ Day_020_SnakePt1/scoreboard.py
@@ -0,0 +1,17 @@
+from turtle import Turtle
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 250)
+        self.hideturtle()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
+
+    def score_add(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
