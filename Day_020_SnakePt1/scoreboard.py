15e11cfe5bcc3d72e6e25a5d3f84ad2591740ba0
naturesbless <lutimoth@usc.edu>
2022-08-30 23:24:22 -0700

    tidy code

--- Day_020_SnakePt1/scoreboard.py
+++ Day_020_SnakePt1/scoreboard.py
@@ -7,11 +7,14 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.penup()
-        self.goto(0, 250)
+        self.goto(0, 270)
         self.hideturtle()
+        self.create_scoreboard()
+
+    def create_scoreboard(self):
         self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
 
     def score_add(self):
         self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 12, "normal"))
+        self.create_scoreboard()
