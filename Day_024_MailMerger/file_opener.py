f3d00b53deefb9157c692c6e29ead6a5b0cccfec
naturesbless <lutimoth@usc.edu>
2022-09-06 21:05:25 -0700

    Writing text

--- Day_024_MailMerger/file_opener.py
+++ Day_024_MailMerger/file_opener.py
@@ -1,4 +1,6 @@
-file = open("pmy_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("my_file.txt", mode='w') as file:
+    file.write("New testing text!")
