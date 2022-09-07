0329020b28cb1e8f32cd4494304eab4042be411d
naturesbless <lutimoth@usc.edu>
2022-09-06 23:05:42 -0700

    add missing states to learn

--- Day_025_StateGuessingGame/main.py
+++ Day_025_StateGuessingGame/main.py
@@ -2,6 +2,7 @@ import turtle
 import pandas as pd
 
 states_info = pd.read_csv("50_states.csv")
+all_states = states_info['state'].to_list()
 
 screen = turtle.Screen()
 screen.title("U.S. State Game")
@@ -26,6 +27,15 @@ writer.hideturtle()
 
 while guessing:
     answer_state = screen.textinput(title=f"{score}/50 States Guessed", prompt="What is a state name?").title()
+    
+    if answer_state == 'Exit':
+        missing_states = []
+        for state in all_states:
+            if state not in correct_guesses:
+                missing_states.append(state)
+        missing_data = pd.DataFrame(missing_states)
+        missing_data.to_csv("states_to_learn.csv")
+        break
 
     if states_info['state'].str.contains(answer_state).any() and answer_state not in correct_guesses:
        info = states_info.loc[states_info["state"] == answer_state].values.flatten().tolist()
@@ -34,6 +44,7 @@ while guessing:
        writer.write(info[0])
        score += 1
        correct_guesses.append(info[0])
+
     if score == 50:
         guessing = False
 
