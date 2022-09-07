c54f0abc61533501d6fb0df030c07fc4d5832fcd
naturesbless <lutimoth@usc.edu>
2022-09-06 21:15:45 -0700

    Create high score file

--- Day_020-021_SnakeGame/scoreboard.py
+++ Day_020-021_SnakeGame/scoreboard.py
@@ -9,12 +9,14 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open('./data.txt') as score_data:
+            self.high_score = int(score_data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
         self.hideturtle()
         self.create_scoreboard()
+        
 
     def create_scoreboard(self):
         self.clear()
@@ -23,6 +25,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open('./data.txt', mode = 'w') as score_data:
+                score_data.write(f"{self.high_score}")
         self.score = 0
         self.create_scoreboard()
 
