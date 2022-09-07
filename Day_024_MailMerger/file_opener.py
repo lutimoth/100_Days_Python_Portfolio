caee77cb64e0f8567e8c75d40387004e1df52498
naturesbless <lutimoth@usc.edu>
2022-09-06 21:06:21 -0700

    Writing files

--- Day_024_MailMerger/file_opener.py
+++ Day_024_MailMerger/file_opener.py
@@ -2,5 +2,11 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("my_file.txt", mode='w') as file:
-    file.write("New testing text!")
+# with open("my_file.txt", mode='w') as file:
+#     file.write("New testing text!")
+
+# with open("my_file.txt", mode='a') as file:
+#     file.write("\n Appending testing text!")
+
+with open("new_file.txt", mode='w') as file:
+    file.write("Just another text file!")
