fb7a0bbcb9e750e5329db43536385996d63c4fb8
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-09-15 16:17:13 -0700

    Updated NATO to Handle Exceptions

--- Day_030_Exceptions/update_nato.py
+++ Day_030_Exceptions/update_nato.py
@@ -2,13 +2,18 @@ import pandas as pd
 
 nato = pd.read_csv('nato_phonetic_alphabet.csv')
 
-#TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
 nato_dict = {row['letter']: row['code'] for (index, row) in nato.iterrows()}
 print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("What is your word?:").upper()
+def generate_nato():
+    word = input("What is your word?:").upper()
 
-nato_word = [nato_dict[letter] for letter in list(word)]
-print(nato_word)
+    try:    
+        nato_word = [nato_dict[letter] for letter in list(word)]
+    except KeyError:
+        print("Only letters please")
+        generate_nato()
+    else:
+        print(nato_word)
+
+generate_nato()
