2735393017e7a2000db7b7bc1d3020673d8cdb7d
naturesbless <lutimoth@usc.edu>
2022-09-14 21:38:38 -0700

    Create label and buttons

--- Day_028_Pomodoro/main.py
+++ Day_028_Pomodoro/main.py
@@ -20,12 +20,21 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro Timer")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=210, height=224)
+timer_text = Label(text="Timer", fg= GREEN, font=(FONT_NAME, 42, "bold"), bg=YELLOW)
+timer_text.grid(column=1, row=0)
+
+canvas = Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="./tomato.png")
 canvas.create_image(103, 112, image=tomato)
-canvas.create_text(103, 130, text="00:00")
-canvas.pack()
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start", font=(FONT_NAME, 12))
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", font=(FONT_NAME,12))
+reset_button.grid(column=2, row=2)
 
 window.mainloop()
