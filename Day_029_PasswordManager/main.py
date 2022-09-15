8952a3664e4e8431140c319d088e8d75eae9c48c
naturesbless <lutimoth@usc.edu>
2022-09-14 22:55:31 -0700

    Tweak UI

--- Day_029_PasswordManager/main.py
+++ Day_029_PasswordManager/main.py
@@ -7,34 +7,42 @@ from tkinter import *
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Generator")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=25)
 
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas = Canvas(width=200, height=200)
 logo = PhotoImage(file="./logo.png")
 canvas.create_image(100, 100, image=logo)
 canvas.grid(column=1, row=0)
 
+
+# Website Info
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_entry = Entry(width=35)
+website_entry = Entry(width=36)
 website_entry.grid(column=1, row=1, columnspan=2)
 
+
+# Email Info
 email_label = Label(text="Email/Username:")
 email_label.grid(column=0, row=2)
 
-email_entry = Entry(width=35)
+email_entry = Entry(width=36)
 email_entry.grid(column=1, row=2, columnspan=2)
 
+
+# Password Info
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
 
-password_text = Entry(width=21)
-password_text.grid(column=1,row=3)
+password_entry = Entry(width=21)
+password_entry.grid(column=1,row=3)
 
 password_gen = Button(text="Generate Password")
 password_gen.grid(column=2, row=3)
 
+
+# Add Button
 add_button = Button(text="Add", width=36)
 add_button.grid(column=1, row=4, columnspan=2)
 
