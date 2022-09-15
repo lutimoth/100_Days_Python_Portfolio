6d47125328a9c5a373452cb07da81febbbd0a8b4
naturesbless <lutimoth@usc.edu>
2022-09-14 22:47:19 -0700

    Create Website and Email Entry

--- Day_029_PasswordManager/main.py
+++ Day_029_PasswordManager/main.py
@@ -12,6 +12,19 @@ window.config(padx=20, pady=20)
 canvas = Canvas(width=200, height=200, highlightthickness=0)
 logo = PhotoImage(file="./logo.png")
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website:")
+website_label.grid(column=0, row=1)
+
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(column=0, row=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(column=1, row=2, columnspan=2)
+
 
 window.mainloop()
