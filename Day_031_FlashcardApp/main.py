f4965d21e1966ada84f86640da035d34c16c476a
naturesbless <lutimoth@usc.edu>
2022-09-18 19:43:05 -0700

    Create card-flip mechanism

--- Day_031_FlashcardApp/main.py
+++ Day_031_FlashcardApp/main.py
@@ -13,25 +13,42 @@ CARD_BACK = './images/card_back.png'
 RIGHT = './images/right.png'
 WRONG = './images/wrong.png'
 
+timer = None
+
 # ---------------------------- Word Generation Mechanism ------------------------------- # 
 french_word = ""
 english_word = ""
 
 def random_words():
+    global words
     words = word_list.sample()
-    french_word, english_word = words.iloc[0]['French'], words.iloc[0]['English']
-    canvas.itemconfig(word_text, text=french_word)
-    return french_word, english_word
+    canvas.itemconfig(card_image, image=card_front_image)
+    french_word = words.iloc[0]['French']
+    canvas.itemconfig(language_text, text="French", fill = 'black')
+    canvas.itemconfig(word_text, text=french_word, fill = 'black')
+    start_timer()
+    return french_word
 
 # ---------------------------- Flash Card Flipping Mech ------------------------------- # 
+def start_timer():
+    global timer
+    timer = window.after(3000, card_flip)
+
+def card_flip():
+    canvas.itemconfig(card_image, image=card_back_image)
+    english_word = words.iloc[0]['English']
+    canvas.itemconfig(language_text, text="English", fill = 'white')
+    canvas.itemconfig(word_text, text=english_word, fill = 'white')
+
     
 # --------------------------------- Flash Card UI ------------------------------------- # 
 window = Tk()
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 canvas = Canvas(width=800, height=526, bg= BACKGROUND_COLOR, highlightthickness=0)
-card_image = PhotoImage(file=CARD_FRONT)
-canvas.create_image(400, 263, image=card_image)
+card_front_image = PhotoImage(file=CARD_FRONT)
+card_back_image = PhotoImage(file=CARD_BACK)
+card_image = canvas.create_image(400, 263, image=card_front_image)
 language_text = canvas.create_text(400, 150, text="French", font=(FONT_NAME, 40, "italic"))
 word_text = canvas.create_text(400, 263, text="Press to Begin", font=(FONT_NAME, 60, "italic"))
 canvas.grid(row=0, column=0, columnspan=2)
