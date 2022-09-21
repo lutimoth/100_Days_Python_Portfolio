af66524275f6476752611e0cb5211c81614e3467
naturesbless <lutimoth@usc.edu>
2022-09-20 22:00:29 -0700

    Clean up connection code

--- Day_032_EmailAutomator/main.py
+++ Day_032_EmailAutomator/main.py
@@ -1,9 +1,16 @@
 import smtplib
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 my_email = "tlupython@gmail.com"
 password = os.getenv('PASSWORD')
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user= my_email, password=password)
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email, 
+        to_addrs='lutimoth@gmail.com', 
+        msg="Subject: Hello \n\nOh what a test! Can we Python it?")
