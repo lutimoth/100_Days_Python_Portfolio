348d7c2ad96cb644c80e6f18ee318470838d1708
naturesbless <lutimoth@usc.edu>
2022-09-20 22:51:53 -0700

    Generalize for emails

--- Day_032_EmailAutomator/main.py
+++ Day_032_EmailAutomator/main.py
@@ -5,11 +5,24 @@ import random
 import smtplib
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
+# Set up email environment
+# Get today's date
 my_email = "tlupython@gmail.com"
 password = os.getenv('PASSWORD')
 today = pd.Timestamp.now().strftime('%m-%d')
+
+# Read the CSV of all birthdays
+birthdays = pd.read_csv('birthdays.csv')
+
+# Convert separate month and days into singular birthdate
+birthdays['birthdate'] = birthdays.apply(lambda x:dt.datetime.strptime("{0} {1}".format(x['month'], x['day']), "%m %d"),axis=1)
+birthdays.to_csv('test_birthday.csv')
+
+# Filter dataframe for only birthdays which match today
+today_df = birthdays.loc[birthdays['birthdate'].dt.strftime('%m-%d').eq(today)]
 
 # Function which will help us rewrite letter template
 def letter_fixer(letter_number):
@@ -20,23 +33,11 @@ def letter_fixer(letter_number):
     final_letter = ' '.join(standard_letter)
     return final_letter
 
-# Read the CSV of all birthdays
-birthdays = pd.read_csv('birthdays.csv')
-
-# Convert separate month and days into singular birthdate
-birthdays['birthdate'] = birthdays.apply(lambda x:dt.datetime.strptime("{0} {1}".format(x['month'], x['day']), "%m %d"),axis=1)
-birthdays.to_csv('test_birthday.csv')
-
-
-
-# Filter dataframe for only birthdays which match today
-today_df = birthdays.loc[birthdays['birthdate'].dt.strftime('%m-%d').eq(today)]
-
-
-
+# For each birthday in the dataframe, iterate and pick a random letter template
 for index, people in today_df.iterrows():
     letter = random.randint(0,2)
     name = people['name']
+    email = people['email']
     if letter == 0:
         letter_fixer(1)
     elif letter == 1:
@@ -44,13 +45,13 @@ for index, people in today_df.iterrows():
     else:
         letter_fixer(3)
 
-# 4. Send the letter generated in step 3 to that person's email address.
+# Send letter to person's address
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(
             from_addr=my_email, 
-            to_addrs='lutimoth@gmail.com', 
+            to_addrs=email, 
             msg=f"Subject: Happy Birthday\n\n {final_letter}")
 
 
