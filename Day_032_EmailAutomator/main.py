d33a017dedd6caf902b473f7ae4981915a6ec0fb
naturesbless <lutimoth@usc.edu>
2022-09-20 21:49:22 -0700

    import env with password

--- Day_032_EmailAutomator/main.py
+++ Day_032_EmailAutomator/main.py
@@ -1,7 +1,9 @@
 import smtplib
+import os
 
 my_email = "tlupython@gmail.com"
+password = os.getenv('PASSWORD')
 
 connection = smtplib.SMTP("smtp.gmail.com")
 connection.starttls()
-connection.login(user=, password=)
+connection.login(user= my_email, password=password)
