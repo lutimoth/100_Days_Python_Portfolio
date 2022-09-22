5b9b0a5d1f6bd9a12e9d0281ea81d3fe86472cbe
naturesbless <lutimoth@usc.edu>
2022-09-22 00:19:17 -0700

    Creating scheduler

--- Day_033_ISS-API/main.py
+++ Day_033_ISS-API/main.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import smtplib
 import os
 from dotenv import load_dotenv
+import sched
+import time
 
 load_dotenv()
 
@@ -12,13 +14,6 @@ password = os.getenv('PASSWORD')
 MY_LAT = 34.069210 # Your latitude
 MY_LONG = -118.009360 # Your longitude
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
 #Your position is within +5 or -5 degrees of the ISS position.
 parameters = {
     "lat": MY_LAT,
@@ -26,45 +21,82 @@ parameters = {
     "formatted": 0
 }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-time_now = datetime.now()
-
 #If the ISS is close to my current position
 def iss_close():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
     if abs(iss_longitude - MY_LONG) <= 5 and abs(iss_latitude - MY_LAT) <= 5:
         return True
     else:
         return False
 
 # and it is currently dark
-def check_night():
+def is_night():
+    time = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    time.raise_for_status()
+    data = time.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+    time_now = datetime.now()
     if time_now.hour >= sunset or time_now.hour <= sunrise:
         return True
     else:
         return False
 
 # Then send me an email to tell me to look up.
+def iss_emailer():
+    iss_close()
+    is_night()
 
-if iss_close() and check_night():
-     with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=password)
-        connection.sendmail(
-            from_addr=my_email, 
-            to_addrs='lutimoth@gmail.com', 
-            msg=f"Subject: Quote of the Day\n\n {today_quote}")
-elif iss_close():
-    print("ISS close but not dark")
-elif check_night():
-    print("It's night but ISS is not close")
-else:
-    print("ISS is not close and it's not dark")
+    if iss_close() and is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email, 
+                to_addrs='lutimoth@gmail.com', 
+                msg=f"Subject: Look up\n\n ISS is right above tonight, check it out!")
+    elif iss_close():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email, 
+                to_addrs='lutimoth@gmail.com', 
+                msg=f"Subject: Too bad\n\n ISS is close but it's bright")
+    elif is_night():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email, 
+                to_addrs='lutimoth@gmail.com', 
+                msg=f"Subject: Nothing up there\n\n It's night but ISS is not up there")
+    else:
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email, 
+                to_addrs='lutimoth@gmail.com', 
+                msg=f"Subject: Don't wait up\n\n ISS is not close and it's bright")
+    
+while is_night:
+    time.sleep(60)
+    iss_emailer()
+    # sc.enter(60, 1, iss_emailer, (sc,))
+    # part of the scheduler, need to put sc as arg in function for it to work
+
 # BONUS: run the code every 60 seconds.
+# s = sched.scheduler(time.time, time.sleep)
+# s.enter(60, 1, iss_emailer, (s,))
+# s.run()
+
 
 
 
