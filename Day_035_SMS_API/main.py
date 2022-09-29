a7dd8e0394abef18a84934b786ac2c321cf593b6
naturesbless <lutimoth@usc.edu>
2022-09-28 22:06:06 -0700

    exclude unecessary data

--- Day_035_SMS_API/main.py
+++ Day_035_SMS_API/main.py
@@ -7,9 +7,10 @@ from dotenv import getenv
 OWM_Endpoint = "https://api.openweathermap.org/data/3,0/onecall"
 
 weather_params = {
-    "lat": 34.069210
-    "long": -118.009361
-    "appid": api_key
+    "lat": 34.069210,
+    "long": -118.009361,
+    "appid": api_key,
+    "exclude": "current, minutely, daily"
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
