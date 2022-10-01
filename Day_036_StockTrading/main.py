48cbc0367522f704bd1e436a89ad7ec874d47d9f
naturesbless <lutimoth@usc.edu>
2022-10-01 00:09:32 -0700

    Get Twilio news and change notifications

--- Day_036_StockTrading/main.py
+++ Day_036_StockTrading/main.py
@@ -1,13 +1,15 @@
 import os
 from dotenv import load_dotenv
-from datetime import datetime, date, timedelta
+from datetime import date, timedelta
 import requests
-from collections import defaultdict
+from twilio.rest import Client
 
 load_dotenv()
 
 alpha_key = os.getenv('ALPHA_KEY')
 news_key = os.getenv('NEWS_KEY')
+twilio_sid = os.getenv('TWILIO_SID')
+twilio_auth = os.getenv('TWILIO_AUTH')
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -19,8 +21,8 @@ YESTERDAY = CURRENT_DAY - timedelta(days=1)
 def price_change(today, yesterday):
     open = float(today['1. open'])
     close = float(yesterday['4. close'])
-    change = (abs(open-close)/close) * 100
-    if change > 5:
+    change = (open-close/close) * 100
+    if change > 5 or change < -5:
         get_news()
     return change
 
@@ -30,7 +32,7 @@ data = stock_request.json()['Time Series (Daily)']
 today_data = data[CURRENT_DAY.strftime('%Y-%m-%d')]
 yesterday_data = data[YESTERDAY.strftime('%Y-%m-%d')]
 
-print(price_change(today_data, yesterday_data))
+percent_change = price_change(today_data, yesterday_data)
 
 ## STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
@@ -41,21 +43,42 @@ def get_news():
     news_info = {}
     news_request = requests.get(news_url)
     news_data = news_request.json()['articles'][:3]
-    #print(news_data)
     for article in news_data:
         if article['source']['name'] not in news_info:
             news_info[article['source']['name']] = [article['title']]
         else:
             news_info[article['source']['name']] += [article['title']]
-        #print(news_info)
-    return news_info
-
-print(get_news())
+    send_message(news_info)
 
 ## STEP 3: Use https://www.twilio.com
 # Send a seperate message with the percentage change and each article's title and description to your phone number. 
+def send_message(articles):
+    client = Client(twilio_sid, twilio_auth)
+    if percent_change > 5:
+        message = client.messages \
+                    .create(
+                        body= f"🔺TSLA: {percent_change}%",
+                        from_='+17407154094',
+                        to='+16266794568'
+                    )
+    if percent_change < 5:
+        message = client.messages \
+                    .create(
+                        body= f"🔻TSLA: {percent_change}%",
+                        from_='+17407154094',
+                        to='+16266794568'
+                    )
+    print(articles)
+    for agency in articles:
+        for title in articles[agency]:
+            message = client.messages \
+                    .create(
+                        body= f"{agency}: {title}",
+                        from_='+17407154094',
+                        to='+16266794568'
+                    )
 
-
+get_news()
 
 #Optional: Format the SMS message like this: 
 """
