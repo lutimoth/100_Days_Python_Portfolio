7dccb25acd9ace26a896499ae8f452fa094f11dd
naturesbless <lutimoth@usc.edu>
2022-10-03 23:59:19 -0700

    setup pixela user

--- Day_037_HabitTracker/main.py
+++ Day_037_HabitTracker/main.py
@@ -1,3 +1,21 @@
 # habit tracker
-
+import os
+from dotenv import load_dotenv
 import requests
+
+load_dotenv()
+
+pixela_token = os.getenv("PIXELA_TOKEN")
+pixela_user = os.getenv("PIXELA_USER")
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params ={
+    "token":pixela_token,
+    "username": pixela_user,
+    "agreeTermsOfService":"yes",
+    "notMinor":"yes"
+}
+
+r = requests.post(url = pixela_endpoint, json= user_params)
+print(r.text)
