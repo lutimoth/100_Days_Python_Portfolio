e8de70e312cae99d32362ee1c556964da0c213e7
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-10-06 14:52:19 -0700

    Update main script

--- Day_040_FlightClub/main.py
+++ Day_040_FlightClub/main.py
@@ -3,6 +3,7 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 from notification_manager import NotificationManager
 
+
 ORIGIN_CITY_IATA = "LON"
 
 data_manager = DataManager()
@@ -13,23 +14,35 @@ sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "":
     city_names = [row["city"] for row in sheet_data]
-    print(city_names)
-    codes = flight_search.get_destination_codes(city_names)
-    data_manager.update_destination_codes(codes)
+    data_manager.city_codes = flight_search.get_destination_codes(city_names)
+    data_manager.update_destination_codes()
     sheet_data = data_manager.get_destination_data()
 
-today = datetime.now() + timedelta(1)
-six_month_from_today = datetime.now() + timedelta(6 * 30)
+destinations = {
+    data["iataCode"]: {
+        "id": data["id"],
+        "city": data["city"],
+        "price": data["lowestPrice"]
+    } for data in sheet_data}
 
-for destination in sheet_data:
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=6 * 30)
+
+for destination_code in destinations:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
-        destination["iataCode"],
-        from_time=today,
+        destination_code,
+        from_time=tomorrow,
         to_time=six_month_from_today
     )
+    if flight is None:
+        continue
 
-    if flight.price < destination["lowestPrice"]:
-        notification_manager.send_sms(
-            message=f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
-        )
+    if flight.price < destinations[destination_code]["price"]:
+        message = f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+
+        if flight.stop_overs > 0:
+            message += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+            print(message)
+
+        notification_manager.send_sms(message)
