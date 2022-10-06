5af3c4b996699f03bca2f90fffe895219854d456
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-10-06 15:06:16 -0700

    update env and email system

--- Day_040_FlightClub/notification_manager.py
+++ Day_040_FlightClub/notification_manager.py
@@ -1,4 +1,5 @@
 from twilio.rest import Client
+import smtplib
 import os
 from dotenv import load_dotenv
 
@@ -8,7 +9,9 @@ TWILIO_SID = os.getenv("TWILIO_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH")
 TWILIO_VIRTUAL_NUMBER = os.getenv("TWILIO_NUMBER")
 TWILIO_VERIFIED_NUMBER = os.getenv("TWILIO_SEND")
-
+MAIL_PROVIDER_SMTP_ADDRESS = "smtp.gmail.com"
+MY_EMAIL = "tlupython@gmail.com"
+MY_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 class NotificationManager:
 
@@ -21,5 +24,14 @@ class NotificationManager:
             from_=TWILIO_VIRTUAL_NUMBER,
             to=TWILIO_VERIFIED_NUMBER,
         )
-        # Prints if successfully sent.
         print(message.sid)
+
+    def send_emails(self, emails, message, google_flight_link):
+        with smtplib.SMTP(MAIL_PROVIDER_SMTP_ADDRESS) as connection:
+            connection.starttls()
+            connection.login(MY_EMAIL, MY_PASSWORD)
+            for email in emails:
+                connection.sendmail(
+                    from_addr=MY_EMAIL,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode('utf-8')
