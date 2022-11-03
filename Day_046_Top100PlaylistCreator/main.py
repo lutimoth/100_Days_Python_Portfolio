3122ef0a6b59730c9e57670b8ea45df84837e71e
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-03 15:09:58 -0700

    create spotipy

--- Day_046_Top100PlaylistCreator/main.py
+++ Day_046_Top100PlaylistCreator/main.py
@@ -2,14 +2,25 @@ from bs4 import BeautifulSoup
 import requests
 import os
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
-date = input('Which year do you want to travel to? Must in in format YYYY-MM-DD: ')
+load_dotenv()
 
-billboard = requests.get(f'https://www.billboard.com/charts/hot-100/{date}/')
+SPOTIPY_CLIENT_ID = os.environ['SPOTIPY_CLIENT_ID']
+SPOTIPY_CLIENT_SECRET = os.environ['SPOTIPY_CLIENT_SECRET']
 
-soup = BeautifulSoup(billboard.text, 'html.parser')
+scope="user-library-read"
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-songlist = soup.select("li ul li h3")
 
-song_names = [song.get_text(strip=True) for song in songlist]
-print(song_names)
+# date = input('Which year do you want to travel to? Must in in format YYYY-MM-DD: ')
+
+# billboard = requests.get(f'https://www.billboard.com/charts/hot-100/{date}/')
+
+# soup = BeautifulSoup(billboard.text, 'html.parser')
+
+# songlist = soup.select("li ul li h3")
+
+# song_names = [song.get_text(strip=True) for song in songlist]
+# print(song_names)
