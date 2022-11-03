9d5a793361eec0a292a130473530cf305c0c0db6
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-03 16:38:50 -0700

    Finish with spotif

--- Day_046_Top100PlaylistCreator/main.py
+++ Day_046_Top100PlaylistCreator/main.py
@@ -12,7 +12,7 @@ SPOTIPY_CLIENT_ID = os.environ['SPOTIPY_CLIENT_ID']
 SPOTIPY_CLIENT_SECRET = os.environ['SPOTIPY_CLIENT_SECRET']
 SPOTIPY_REDIRECT_URI = os.environ["SPOTIPY_REDIRECT_URI"]
 
-scope="playlist-modify-private"
+scope="playlist-modify-public"
 auth_manager = SpotifyOAuth(scope=scope, 
                             redirect_uri=SPOTIPY_REDIRECT_URI, 
                             client_id=SPOTIPY_CLIENT_ID, 
@@ -26,22 +26,22 @@ user_id = sp.current_user()["id"]
 
 ### ==== BILLBOARD SONG LIST AND URI LIST ==== ###
 
-#date = input('Which year do you want to travel to? Must in in format YYYY-MM-DD: ')  # Ask date
-#billboard = requests.get(f'https://www.billboard.com/charts/hot-100/{date}/') # Request info from Billboard for date
+date = input('Which year do you want to travel to? Must in in format YYYY-MM-DD: ')  # Ask date
+billboard = requests.get(f'https://www.billboard.com/charts/hot-100/{date}/') # Request info from Billboard for date
 
-#soup = BeautifulSoup(billboard.text, 'html.parser') # Parse with Beautiful Soup
+soup = BeautifulSoup(billboard.text, 'html.parser') # Parse with Beautiful Soup
  
-# songlist = soup.select("li ul li h3")  # Get songlist from class hierarchy
-# song_names = [song.get_text(strip=True) for song in songlist]  # Get song names only
+songlist = soup.select("li ul li h3")  # Get songlist from class hierarchy
+song_names = [song.get_text(strip=True) for song in songlist]  # Get song names only
 
-# song_info = [sp.search(q='track:'+song, type='track') for song in song_names]  # Use spotipy to search for songs
+song_info = [sp.search(q='track:'+song, type='track') for song in song_names]  # Use spotipy to search for songs
 
-# song_uri = []  # Empty list for track URIs
-# for song in song_info:
-#     try:  
-#         song_uri.append(song['tracks']['items'][0]['uri']) 
-#     except IndexError:  # Some songs may not exist, skip these
-#         continue 
+song_uri = []  # Empty list for track URIs
+for song in song_info:
+    try:  
+        song_uri.append(song['tracks']['items'][0]['uri']) 
+    except IndexError:  # Some songs may not exist, skip these
+        continue 
 
 # print(song_uri)
 # print(song_uri[0])
@@ -50,9 +50,9 @@ user_id = sp.current_user()["id"]
 
 ### === PLAYLIST CREATION === ###
 
-#sp.user_playlist_create(user=user_id, name=f"Billboard {date} Top 100 Songs", public=False)
+sp.user_playlist_create(user=user_id, name=f"Billboard {date} Top 100 Songs", public=True)
 
 playlists = sp.user_playlists(user=user_id)
-for items in playlists['items']:
-    print(items['name'])
-#sp.user_playlist_add_tracks(user=user_id, playlist_id= , tracks=song_uri)
+playlist_id = playlists['items'][0]['id']
+
+sp.user_playlist_add_tracks(user=user_id, playlist_id=playlist_id, tracks=song_uri)
