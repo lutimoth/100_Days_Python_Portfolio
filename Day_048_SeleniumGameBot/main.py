a59c445fd1857204ca69fa5b691078de85ee8f61
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-07 13:54:41 -0800

    testing chromedriver

--- Day_048_SeleniumGameBot/main.py
+++ Day_048_SeleniumGameBot/main.py
@@ -1,3 +1,7 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-chromedrive_directory = "./chromedriver.exe"
+chromedrive_directory = "C:/Users/lutimoth/Documents/chromedriver/chromedriver.exe"
+driver = webdriver.Chrome(service=Service(chromedrive_directory))
+
+driver.get("https://www.amazon.com")
