3904f955fa53c1cee8516811949f47d900827a1f
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-07 14:31:10 -0800

    test article finder

--- Day_048_SeleniumGameBot/main.py
+++ Day_048_SeleniumGameBot/main.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
 
 chromedrive_directory = "C:/Users/lutimoth/Documents/chromedriver/chromedriver.exe"
 driver = webdriver.Chrome(service=Service(chromedrive_directory))
 
-driver.get("https://www.amazon.com")
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+number_of_articles = driver.find_element(By.CSS_SELECTOR, '#articlecount a')
+print(number_of_articles.text)
+
+driver.quit()
