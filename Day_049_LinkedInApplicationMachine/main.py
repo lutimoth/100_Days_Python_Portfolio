f5500647d1b01b01aed72c4136ea794233d94fc0
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-09 16:07:47 -0800

    first try at auto-apply

--- Day_049_LinkedInApplicationMachine/main.py
+++ Day_049_LinkedInApplicationMachine/main.py
@@ -21,12 +21,12 @@ driver = webdriver.Chrome(service=Service(chromedrive_directory))
 
 driver.get(URL)
 
-time.sleep(10)
+time.sleep(5)
 
 signin_button = driver.find_element(By.CSS_SELECTOR, "a.nav__button-secondary")
 signin_button.click()
 
-time.sleep(10)
+time.sleep(5)
 
 user_fill = driver.find_element(By.ID,'username')
 user_fill.send_keys(USER)
@@ -37,4 +37,23 @@ pw_fill.send_keys(PW)
 login_button = driver.find_element(By.TAG_NAME, 'button')
 login_button.click()
 
-time.sleep(20)
+time.sleep(5)
+
+jobs = driver.find_elements(By.CLASS_NAME, 'job-card-container')
+
+for job in jobs:
+    job.click()
+    time.sleep(3)
+    easy_apply = driver.find_element(By.CLASS_NAME, 'jobs-apply-button')
+    easy_apply.click()
+    time.sleep(5)
+    easy_text = driver.find_element(By.CLASS_NAME, 'artdeco-button__text')
+    
+    if easy_text.text == 'Submit Application':
+        submit_button = driver.find_element(By.CLASS_NAME, 'artdeco-button')
+        submit_button.click()
+        time.sleep(5)
+        submit_button.click()
+        time.sleep(5)
+    else:
+        cancel_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismissal')
