546c1428132b0e9e2c12297d7181b4bdda0396d1
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-09 16:39:57 -0800

    figure out last number for pages

--- Day_049_LinkedInApplicationMachine/main.py
+++ Day_049_LinkedInApplicationMachine/main.py
@@ -21,12 +21,12 @@ driver = webdriver.Chrome(service=Service(chromedrive_directory))
 
 driver.get(URL)
 
-time.sleep(5)
+time.sleep(2)
 
 signin_button = driver.find_element(By.CSS_SELECTOR, "a.nav__button-secondary")
 signin_button.click()
 
-time.sleep(5)
+time.sleep(2)
 
 user_fill = driver.find_element(By.ID,'username')
 user_fill.send_keys(USER)
@@ -37,34 +37,40 @@ pw_fill.send_keys(PW)
 login_button = driver.find_element(By.TAG_NAME, 'button')
 login_button.click()
 
-time.sleep(5)
+time.sleep(2)
 
-jobs = driver.find_elements(By.CLASS_NAME, 'job-card-container')
 
-for job in jobs:
-    job.click()
-    time.sleep(3)
-    try:
-        easy_apply = driver.find_element(By.CLASS_NAME, 'jobs-apply-button')
-        easy_apply.click()
-        time.sleep(5)
-        
-        easy_text = driver.find_element(By.CLASS_NAME, 'artdeco-button__text')
-        print(easy_text.text)
-        
-        if easy_text.text == 'Submit Application':
-            submit_button = driver.find_element(By.CLASS_NAME, 'artdeco-button--primary')
-            submit_button.click()
-            time.sleep(5)
-            done_button = driver.find_element(By.CLASS_NAME, 'artdeco-button--primary')
-            done_button.click()
-            time.sleep(5)
-        else:
-            cancel_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
-            cancel_button.click()
-            time.sleep(2)
-            discard_button = driver.find_element(By.CSS_SELECTOR, 'button.artdeco-modal__confirm-dialog-btn.artdeco-button--secondary')
-            discard_button.click()
-            time.sleep(2)
-    except NoSuchElementException:
-        continue
+# def job_hunt():
+#     jobs = driver.find_elements(By.CLASS_NAME, 'job-card-container')
+
+#     for job in jobs:
+#         job.click()
+#         time.sleep(3)
+#         try:
+#             easy_apply = driver.find_element(By.CLASS_NAME, 'jobs-apply-button')
+#             easy_apply.click()
+#             time.sleep(5)
+            
+#             easy_text = driver.find_element(By.CLASS_NAME, 'artdeco-button__text')
+#             print(easy_text.text)
+            
+#             if easy_text.text == 'Submit Application':
+#                 submit_button = driver.find_element(By.CLASS_NAME, 'artdeco-button--primary')
+#                 submit_button.click()
+#                 time.sleep(3)
+#                 done_button = driver.find_element(By.CLASS_NAME, 'artdeco-button--primary')
+#                 done_button.click()
+#                 time.sleep(3)
+#             else:
+#                 cancel_button = driver.find_element(By.CLASS_NAME, 'artdeco-modal__dismiss')
+#                 cancel_button.click()
+#                 time.sleep(2)
+#                 discard_button = driver.find_element(By.CSS_SELECTOR, 'button.artdeco-modal__confirm-dialog-btn.artdeco-button--secondary')
+#                 discard_button.click()
+#                 time.sleep(2)
+#         except NoSuchElementException:
+#             continue
+
+page_numbers = driver.find_elements(By.CLASS_NAME, 'artdeco-pagination__indicator')
+last_number = int(page_numbers[-1].text)
+print(last_number)
