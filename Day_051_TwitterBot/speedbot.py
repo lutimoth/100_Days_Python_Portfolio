f5bfbab86d2c3be3337c54d2c13fecc43ba0c4fe
naturesbless <lutimoth@usc.edu>
2022-11-14 19:53:43 -0800

    tweak down and up collection

--- Day_051_TwitterBot/speedbot.py
+++ Day_051_TwitterBot/speedbot.py
@@ -19,29 +19,31 @@ URL = 'https://www.speedtest.net/'
 
 class InternetSpeedTwitterBot:
 
-    def __init__(self) -> None:
-        self.driver = webdriver.Chrome(service=Service(executable_path=CHROME_DRIVER_PATH))
+    def __init__(self,driver_path) -> None:
+        self.driver = webdriver.Chrome(service=Service(executable_path=driver_path))
         self.down = 0
         self.up = 0
     
     def get_internet_speed(self):
         self.driver.get(URL)
-        time.sleep(5)
+        time.sleep(2)
         self.go_button = self.driver.find_element(By.CSS_SELECTOR, 'a.js-start-test')
         self.go_button.click()
-        time.sleep(30)
-        self.close_button = self.driver.find_element(By.CLASS_NAME, 'close-btn')
-        self.close_button.click()
+        time.sleep(50)
+        # self.close_button = self.driver.find_element(By.CLASS_NAME, 'close-btn')
+        # self.close_button.click()
+        # time.sleep(3)
         self.down = self.driver.find_element(By.CLASS_NAME, 'download-speed')
         self.up = self.driver.find_element(By.CLASS_NAME, 'upload-speed')
-        print(self.down)
-        print(self.up)
 
     def tweet_at_provider(self):
         pass
 
 
-bot = InternetSpeedTwitterBot()
+bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
 
 bot.get_internet_speed()
+print(f"down: {bot.down.text}")
+print(f"up: {bot.up.text}")
+
 bot.tweet_at_provider()
