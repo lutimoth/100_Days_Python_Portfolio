a1eec71e2cd64b7696c894cdab7dd4c63ab11c79
naturesbless <lutimoth@usc.edu>
2022-11-23 18:29:49 -0800

    get start files

--- Day_057_JinjaTemplates/jinja_test/server.py
+++ Day_057_JinjaTemplates/jinja_test/server.py
@@ -9,7 +9,7 @@ def home():
     year = datetime.now().year
     return render_template("index.html", current_year = year)
 
-@app.route(f'/guess/<name>')
+@app.route('/guess/<name>')
 def guess(name):
     year = datetime.now().year
     gender_result = requests.get('https://api.genderize.io', params={'name': name})
@@ -18,6 +18,12 @@ def guess(name):
     age = age_result.json()['age']
     return render_template("guess.html", current_year = year, name=name, gender=gender, age=age)
 
+@app.route("/blog")
+def get_blog():
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts)
 
 if __name__ == "__main__":
     app.run(debug=True)
