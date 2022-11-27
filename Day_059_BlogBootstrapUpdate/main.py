a37770f9f37bbcafcead0c4135b2d3699381f4d3
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-26 16:35:55 -0800

    fix about and contact

---
+++ Day_059_BlogBootstrapUpdate/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact_page():
+    return render_template('contact.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
