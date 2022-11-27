b604ee89d46dff022eb0655a9d04c1a0201ea6f4
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-26 16:41:03 -0800

    create post

--- Day_059_BlogBootstrapUpdate/main.py
+++ Day_059_BlogBootstrapUpdate/main.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+response = requests.get('https://api.npoint.io/908ee30b5c1c9a2037f6')
+blog_posts = response.json()
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', all_posts=blog_posts)
 
 @app.route('/about.html')
 def about_page():
