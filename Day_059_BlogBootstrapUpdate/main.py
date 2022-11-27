fee513cc2fcc7999b9bc5fabd320c956262325da
naturesbless <91281654+naturesbless@users.noreply.github.com>
2022-11-26 16:50:04 -0800

    Finish post linking

--- Day_059_BlogBootstrapUpdate/main.py
+++ Day_059_BlogBootstrapUpdate/main.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 app = Flask(__name__)
 
-response = requests.get('https://api.npoint.io/908ee30b5c1c9a2037f6')
-blog_posts = response.json()
+posts = requests.get('https://api.npoint.io/908ee30b5c1c9a2037f6').json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 @app.route('/')
 def home():
-    return render_template('index.html', all_posts=blog_posts)
+    return render_template('index.html', all_posts=posts)
 
 @app.route('/about.html')
 def about_page():
@@ -17,6 +21,14 @@ def about_page():
 @app.route('/contact.html')
 def contact_page():
     return render_template('contact.html')
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
