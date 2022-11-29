4b0d33602f4f28d3a95ea7bffabf295c47be168d
naturesbless <lutimoth@usc.edu>
2022-11-28 21:20:53 -0800

    Building out flask form

---
+++ Day_060_BlogContactForm/main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+import requests
+from post import Post
+
+app = Flask(__name__)
+
+posts = requests.get('https://api.npoint.io/908ee30b5c1c9a2037f6').json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
+
+@app.route('/')
+def home():
+    return render_template('index.html', all_posts=posts)
+
+@app.route('/about.html')
+def about_page():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact_page():
+    return render_template('contact.html')
+
+@app.route('/form-entry', methods=["POST"])
+def receive_data():
+    data = request.form
+    print(data["name"])
+    print(data["email"])
+    print(data["phone"])
+    print(data["message"])
+    return "<h1>Successfully sent your message</h1>"
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
