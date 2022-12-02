1969e63b6aa58b83a5537519e2cc743866279d35
naturesbless <lutimoth@usc.edu>
2022-12-02 12:30:26 -0800

    create sqlite datrabase

--- Day_062_SQLiteDatabase/main.py
+++ Day_062_SQLiteDatabase/main.py
@@ -1,32 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////new-books-collection.db'
 
-db = sqlite3.connect("./Day_062_SQLiteDatabase/books-collection.db")
-cursor = db.cursor()
+db = SQLAlchemy(app)
 
-# cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, \
-#     author varchar(250) NOT NULL, \
-#     rating FLOAT NOT NULL)")
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(250), unique = True, nullable = False)
+    author = db.Column(db.String(250), unique = True, nullable = False)
+    rating = db.Column(db.Float, nullable = False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+db.create_all()
 
 all_books = []
 
 @app.route('/')
 def home():
+    all_books = db.session.query(Books).all()
     return render_template('index.html', books=all_books)
 
 
 @app.route("/add", methods=["GET","POST"])
 def add():
-    book_info = {
-        'name': request.form.get('book_name'),
-        'author': request.form.get('book_author'),
-        'rating': request.form.get('rating')
-    }
-    all_books.append(book_info)
-    print(all_books)
+    if request.method == "POST":
+        book_info = Books(title=request.form.get('book_name'), author=request.form.get('book_author'), \
+            rating = request.form.get('rating'))
+        db.session.add(book_info)
+        db.session.commit()
+        return redirect(url_for('home'))
     return render_template('add.html')
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        #UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = Books.query.get(book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = Books.query.get(book_id)
+    return render_template("edit_rating.html", book=book_selected)
 
 
 if __name__ == "__main__":
