ed21be2b2fabe028286801891be84fd0946fbb3b
naturesbless <lutimoth@usc.edu>
2022-12-02 12:09:55 -0800

    createing sqlite

--- Day_062_SQLiteDatabase/main.py
+++ Day_062_SQLiteDatabase/main.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
-all_books = []
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
 
+cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, \
+    author varchar(250) NOT NULL, \
+    rating FLOAT NOT NULL)")
+
+all_books = []
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', books=all_books)
 
 
 @app.route("/add", methods=["GET","POST"])
@@ -18,6 +25,7 @@ def add():
         'rating': request.form.get('rating')
     }
     all_books.append(book_info)
+    print(all_books)
     return render_template('add.html')
 
 
