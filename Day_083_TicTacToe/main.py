7db397733d7cf4dccbb25c78eea37e22c441ac50
naturesbless <91281654+naturesbless@users.noreply.github.com>
2023-01-12 12:04:59 -0800

    first iteration of board placement

--- Day_083_TicTacToe/main.py
+++ Day_083_TicTacToe/main.py
@@ -20,6 +20,28 @@ if player_1 == 'X':
 else:
     player_2 = 'X'
 
+def symbol_place(board, row, col, player_symbol):
+    if col == 1:
+        board[row-1][col] = player_symbol
+    if col == 2:
+        board[row-1][4] = player_symbol
+    if col == 3:
+        board[row-1][7] = player_symbol
+    return board
+        
+
 round = 1
 
 while round <= 9:
+    if round%2 != 0:
+        row_1 = int(input("Player 1, what row would you like to place your symbol?"))
+        col_1 = int(input("Player 1, what column would you like to place your symbol?"))
+        board.board = symbol_place(board.board, row_1, col_1, player_1)
+        for _ in board.board:
+            print(_)
+    if round%2 == 0:
+        row_2 = int(input("Player 2, what row would you like to place your symbol?"))
+        col_2 = int(input("Player 2, what column would you like to place your symbol?"))
+        board.board = symbol_place(board.board, row_2, col_2, player_2)
+        for _ in board.board:
+            print(_)
