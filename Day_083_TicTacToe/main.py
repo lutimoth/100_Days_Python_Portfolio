7dd9cd17118a77a63bc13dfe0ac129f04cf6b1b9
naturesbless <91281654+naturesbless@users.noreply.github.com>
2023-01-12 11:49:23 -0800

    Create main.py

---
+++ Day_083_TicTacToe/main.py
@@ -0,0 +1,15 @@
+# We gaming with some tic tac toe
+
+
+## --- Build Board --- ###
+# perhaps build the board as a list of lists
+
+board = [
+    [' | | '],
+    ['-----'],
+    [' | | '],
+    ['-----'],
+    [' | | '],
+]
+
+print(board)
