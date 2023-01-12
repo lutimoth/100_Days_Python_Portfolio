9e34e102ca546264321aceea698a01f28be29493
naturesbless <91281654+naturesbless@users.noreply.github.com>
2023-01-12 11:58:42 -0800

    player roles

--- Day_083_TicTacToe/main.py
+++ Day_083_TicTacToe/main.py
@@ -6,7 +6,20 @@
 
 from board import Board
 
-new_board = Board()
+board = Board()
 
-for _ in new_board.board:
+for _ in board.board:
     print(_)
+    
+## get input
+
+player_1 = input("Would you like to be X or O? ")
+
+if player_1 == 'X':
+    player_2 = 'O'
+else:
+    player_2 = 'X'
+
+round = 1
+
+while round <= 9:
