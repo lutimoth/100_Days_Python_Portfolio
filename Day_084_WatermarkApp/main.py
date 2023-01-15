9d46b344ac7d7d16fc51e714fe986a631ba9ccbc
naturesbless <lutimoth@usc.edu>
2023-01-14 18:49:06 -0800

    image upload

--- Day_084_WatermarkApp/main.py
+++ Day_084_WatermarkApp/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import filedialog
 from PIL import ImageTk, Image
 
 window = Tk()
@@ -10,7 +11,15 @@ FONT = ('times', 18, 'bold')
 title = Label(window, text="Image Watermark Generator", width=30, font=FONT)
 title.grid(row=1,column=1)
 
-upload_button = Button(window, text="Upload Image")
+upload_button = Button(window, text="Upload Image", command=lambda:upload_file())
 upload_button.grid(row=2,column=1)
+
+def upload_file():
+    global img
+    file_types = [('Jpg files','*.jpg')]
+    file_name = filedialog.askopenfilename(filetypes=file_types)
+    img = ImageTk.PhotoImage(file=file_name)
+    b2 = Button(window, image=img) # using Button 
+    b2.grid(row=3,column=1)
 
 window.mainloop()
