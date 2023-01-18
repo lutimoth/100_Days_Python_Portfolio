3a2e26963721f60af50d9099440e5e088aa0d9b8
naturesbless <91281654+naturesbless@users.noreply.github.com>
2023-01-18 12:25:38 -0800

    Random phrases and entry box

--- Day_085_TypingTest/main.py
+++ Day_085_TypingTest/main.py
@@ -1,6 +1,25 @@
 from tkinter import *
+from phraselist import phrases
+import random
 
 window = Tk()
 window.minsize(width=800, height=600)
+
+phrase = random.choice(phrases)
+
+# def key(event):
+#      text= event.char
+#      text+= event.char
+#      print(text)
+
+# frame = Frame(window, width=100, height=100)
+# frame.bind("<Key>", key)
+# frame.pack()
+
+phrase_label = Label(window, text=phrase)
+phrase_label.pack()
+
+type_box = Entry(window)
+type_box.pack()
 
 window.mainloop()
---
+++ Day_085_TypingTest/phraselist.py
@@ -0,0 +1,5 @@
+phrases = [
+    "This is a quick little test phrase.",
+    "Quick brown fox jumped over the candlestick.",
+    "This is a third test phrase, wow."
+]
