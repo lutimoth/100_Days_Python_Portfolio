5f3085eb85c63e7cf74c3e57e87f64ccc24904fa
naturesbless <91281654+lutimoth@users.noreply.github.com>
2023-01-19 15:42:53 -0800

    testing code in notebook

--- Day_091_ColorPaletteGenerator/main.py
+++ Day_091_ColorPaletteGenerator/main.py
@@ -1,4 +1,12 @@
-import numpy
+import numpy as np
 import matplotlib.image as img
 import matplotlib.pyplot as plt
+import pandas as pd
 
+img = img.imread('milfordtest.jpg')
+# print(img)
+# print(img.shape)
+color, count = np.unique(img.reshape(-1, img.shape[-1]), axis=0, return_counts=True)
+
+color_df = pd.DataFrame({'color':color.reshape(-1),'count':count})
+print(color_df)
