6deb7f009c6e7ca967a95ec9d49d045f214718cb
naturesbless <91281654+lutimoth@users.noreply.github.com>
2023-02-06 11:08:41 -0800

    Clean up file

--- Day_087_CoffeeWebsiteV2/main.py
+++ Day_087_CoffeeWebsiteV2/main.py
@@ -20,15 +20,6 @@ class CafeForm(FlaskForm):
     power = SelectField('Power Socket Availability', choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
-
 # all Flask routes below
 @app.route("/")
 def home():
@@ -48,9 +39,6 @@ def add_cafe():
                         f"{form.wifi.data},"
                         f"{form.power.data}")
         return redirect(url_for('cafes'))
-    # Exercise:
-    # Make the form write a new row into cafe-data.csv
-    # with   if form.validate_on_submit()
     return render_template('add.html', form=form)
 
 
